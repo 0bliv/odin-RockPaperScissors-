288a42f8f038c1fb7bcbe4f38c4d3eee48343ae5
noivilbo <88675619+noivilbo@users.noreply.github.com>
2024-08-14 10:59:32 -0300

    Added basic structure of the game

--- main.js
+++ main.js
@@ -6,11 +6,75 @@ function getComputerChoice() {
 	return x
 }
 
-function getHumanChoise(){
+function getHumanChoice(){
 	const Escolha_Humano = window.prompt("Escolha sua jogada:")
 	return Escolha_Humano
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoise())
+var humanScore = 0
+var computerScore = 0
+
+function playround(humanChoice,computerChoice){
+	if (humanChoice == computerChoice) {
+		console.log("Draw")
+	}
+
+	if (humanChoice == "Pedra") {
+		if (computerChoice == "Papel") {
+			// console.log("Computador venceu!")
+			computerScore+=1
+		}
+		else {
+			// console.log("Humano venceu!")
+			humanScore+=1
+		}
+	}
+		if (humanChoice == "Papel") {
+		if (computerChoice == "Tesoura") {
+			// console.log("Computador venceu!")
+			computerScore+=1
+		}
+		else {
+			// console.log("Humano venceu!")
+			humanScore+=1
+		}
+	}
+		if (humanChoice == "Tesoura") {
+		if (computerChoice == "Pedra") {
+			// console.log("Computador venceu!")
+			computerScore+=1
+		}
+		else {
+			// console.log("Humano venceu!")
+			humanScore+=1
+		}
+
+	}
+
+}
+
+function playGame(){
+	for (var i = 0; i < 5; i++) 
+	{
+		playround(getHumanChoice(),getComputerChoice())
+	}
+
+	if (humanScore > computerScore) {
+		console.log("Humano venceu!")
+		return humanScore
+	}
+	else if (computerScore > humanScore) {
+		console.log("Computador venceu!")
+		return computerScore
+	}
+	else {
+		console.log("Draw")
+		return 0
+	}
+}
+	
+
+
+console.log(playGame())
+
 
